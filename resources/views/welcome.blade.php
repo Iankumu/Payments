8c3cf80f41fe8655cc40bdd0ad6b3cb683a7fcb9
IanKumu <ikariuki741@gmail.com>
2023-01-12 17:26:14 +0300

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Payments</title>
 </head>
 <body>
 @if(session('success_message'))
