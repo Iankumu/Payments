eeac9cea9ce5781c2566eb5bcdb92bac112ac99b
Ian Kumu <ikariuki741@gmail.com>
2023-02-28 14:57:13 +0300

    cleanup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,17 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <title>{{ config('app.name') }}</title>
 
         <!-- Scripts -->
         @routes
         @vite('resources/js/app.js')
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body >
         @inertia
     </body>
 </html>
