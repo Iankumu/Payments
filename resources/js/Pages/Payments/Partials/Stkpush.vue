11633ceb320df757ea8aa5081b45bc752f7cd5cd
Ian Kumu <ikariuki741@gmail.com>
2024-05-11 02:36:40 +0800

    change

--- resources/js/Pages/Payments/Partials/Stkpush.vue
+++ resources/js/Pages/Payments/Partials/Stkpush.vue
@@ -76,7 +76,7 @@
                                 id="floating_shortcode"
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                 placeholder=" "
-                                v-model="queryForm.checkoutRequestID"
+                                v-model="queryForm.CheckoutRequestID"
                                 required
                             />
                             <label
@@ -119,7 +119,7 @@ const form = reactive({
 });
 
 const queryForm = reactive({
-    checkoutRequestID: "",
+    CheckoutRequestID: "",
 });
 
 defineProps({
